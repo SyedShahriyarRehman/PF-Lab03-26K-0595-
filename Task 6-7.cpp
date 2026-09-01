4bc5aee569540bc778dd440a0fceeddfdcf8829d
SyedShahriyarRehman <syedshahriyarrehman@gmail.com>
2026-09-01 22:14:50 +0500

    lab3-code works well

---
+++ Task 6-7.cpp
@@ -0,0 +1,37 @@
+// Task #6
+#include <stdio.h>
+int main() {
+    printf("Hello World");    
+    return 0;
+}
+
+
+// Task #7
+#include <stdio.h>
+int main() {
+	int age;
+	float height;
+	printf("Enter Your Age and Height: ");
+	scanf("%d %f" , &age , &height);
+	printf("You are %d years old and %.2f meters tall.\n" , age , height);
+	
+	return 0;	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
