54226e2f14cb602323390eed384ecf52a179ea3a
SyedShahriyarRehman <syedshahriyarrehman@gmail.com>
2026-09-05 00:45:17 +0500

    Delete Task 3.c

--- Task 3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    char Name[] = "John Kennedy";
-    char RollNo[] = "26K-0567" ;
-    char Department[] = "BCS";
-    printf("Name of the Student is %s\\His RollNumber is \"%s\"\\His Department is \"%s\"\a\t\n" ,Name,RollNo,Department);
-
-    return 0;
-}
